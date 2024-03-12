c24381042fdf790cc401171c9e67454fe274fbaa
Aulia Harvy <auliaharvy@auliaharvy.local>
2024-03-12 20:18:08 +0700

    fix dashboard forecast

--- app/Http/Controllers/Backend/BackendController.php
+++ app/Http/Controllers/Backend/BackendController.php
@@ -265,9 +265,26 @@ class BackendController extends Controller
             ];
 
             // Mendapatkan data forecast document
-            $forecast_data = DocumentSchedule::leftJoin('schedule_pics', function ($join) {
+            $forecast_data_pic = DocumentSchedule::leftJoin('schedule_pics', function ($join) {
                 $join->on('document_schedules.id', '=', 'schedule_pics.document_schedule_id')
                      ->where('schedule_pics.user_pic_id', auth()->id());  // Filter within the join itself
+            })
+            ->leftJoin('documents', 'document_schedules.document_id', '=', 'documents.id')
+            ->where('documents.user_id', '!=', auth()->id())
+            ->select(
+                'documents.id',  // Included in GROUP BY
+                DB::raw('YEAR(schedule_date) AS year'),
+                DB::raw('MONTH(schedule_date) AS month'),
+                DB::raw('COUNT(*) AS total_documents')
+            )
+            ->groupBy('documents.id', DB::raw('YEAR(schedule_date)'), DB::raw('MONTH(schedule_date)'))  // Group by document ID, year, and month
+            ->orderBy(DB::raw('YEAR(schedule_date)'), 'asc')
+            ->orderBy(DB::raw('MONTH(schedule_date)'), 'asc')
+            ->get();
+
+            $forecast_data_owner = DocumentSchedule::leftJoin('schedule_pics', function ($join) {
+                $join->on('document_schedules.id', '=', 'schedule_pics.document_schedule_id')
+                     ->where('schedule_pics.user_pic_id', '!=',auth()->id());  // Filter within the join itself
             })
             ->leftJoin('documents', 'document_schedules.document_id', '=', 'documents.id')
             ->where('documents.user_id', auth()->id())
@@ -284,7 +301,14 @@ class BackendController extends Controller
         }
 
         $grouped_forecast_data = [];
-        foreach ($forecast_data as $data) {
+        foreach ($forecast_data_pic as $data) {
+            $grouped_forecast_data[$data->year][] = [
+                'month' => $data->month,
+                'total_documents' => $data->total_documents,
+            ];
+        }
+
+        foreach ($forecast_data_owner as $data) {
             $grouped_forecast_data[$data->year][] = [
                 'month' => $data->month,
                 'total_documents' => $data->total_documents,
@@ -295,7 +319,7 @@ class BackendController extends Controller
 
         return view(
             "backend.index",
-            compact('locations', 'departments', 'documenttypes', 'count_document', 'forecast_data', 'grouped_forecast_data')
+            compact('locations', 'departments', 'documenttypes', 'count_document', 'forecast_data_owner', 'grouped_forecast_data')
         );
     }
 
@@ -307,123 +331,7 @@ class BackendController extends Controller
      */
     public function index_filter(Request $request)
     {
-        $currentYear = Carbon::now()->year;
-        $startYear = $currentYear - 1;
-        $endYear = $currentYear + 1;
-        $years = range($startYear, $endYear);
-        $userId = Auth::id();
-        $user = auth()->user();
-        $department_model = $this->department_model;
-        $documenttype_model = $this->documenttype_model;
-        $document_model = $this->document_model;
 
-        // Mendpatkan data document Type
-        $documenttypes = $documenttype_model::select('name', 'id')->get();
-
-
-        if ($user->hasRole('super admin')) {
-            // Mendpatkan data department
-            $departments = $department_model::select('name', 'id')->get();
-
-            // Mendapatkan data count document
-            $total_document = $document_model::Where(function ($query) use($checkbox) {
-                for ($i = 0; $i < count($checkbox ); $i++){
-                      $query->orwhere('department_name', 'like',  '%' . $checkbox [$i] .'%');
-            }})->count();
-            $total_active_document = $document_model::where('status', 1)->count();
-            $total_waiting_document = $document_model::where('status', 2)->count();
-            $total_expired_document = $document_model::where('status', 3)->count();
-            $count_document = [
-                'total' => 2,
-                'active' => $total_active_document,
-                'waiting' => $total_waiting_document,
-                'expired' => $total_expired_document,
-            ];
-
-            // Mendapatkan data forecast document
-            $forecast_data = DB::table('document_schedules')
-            ->select(
-                DB::raw('YEAR(schedule_date) AS year'),
-                DB::raw('MONTH(schedule_date) AS month'),
-                DB::raw('COUNT(*) AS total_documents')
-            )
-            ->whereIn(DB::raw('YEAR(schedule_date)'), [date('Y'), date('Y') + 1])
-            ->groupBy(DB::raw('YEAR(schedule_date)'), DB::raw('MONTH(schedule_date)'))
-            ->orderBy(DB::raw('YEAR(schedule_date)'), 'asc')
-            ->orderBy(DB::raw('MONTH(schedule_date)'), 'asc')
-            ->get();
-
-        } else {
-            // Mendpatkan data department
-            $departments = $department_model::select('name', 'id')->where('id', $user->department_id)->get();
-
-            // Mendapatkan data count document
-            $total_document = $document_model::join('document_schedules', 'documents.id', '=', 'document_schedules.document_id')
-                ->join('schedule_pics', 'document_schedules.id', '=', 'schedule_pics.document_schedule_id')
-                ->where('documents.user_id', $userId)
-                ->where('schedule_pics.user_pic_id', $userId)
-                ->count();
-
-            $total_active_document = $document_model::join('document_schedules', 'documents.id', '=', 'document_schedules.document_id')
-                ->join('schedule_pics', 'document_schedules.id', '=', 'schedule_pics.document_schedule_id')
-                ->where('documents.user_id', $userId)
-                ->where('schedule_pics.user_pic_id', $userId)
-                ->where('documents.status', 1)
-                ->count();
-
-            $total_waiting_document = $document_model::join('document_schedules', 'documents.id', '=', 'document_schedules.document_id')
-                ->join('schedule_pics', 'document_schedules.id', '=', 'schedule_pics.document_schedule_id')
-                ->where('documents.user_id', $userId)
-                ->where('schedule_pics.user_pic_id', $userId)
-                ->where('documents.status', 2)
-                ->count();
-
-            $total_expired_document = $document_model::join('document_schedules', 'documents.id', '=', 'document_schedules.document_id')
-                ->join('schedule_pics', 'document_schedules.id', '=', 'schedule_pics.document_schedule_id')
-                ->where('documents.user_id', $userId)
-                ->where('schedule_pics.user_pic_id', $userId)
-                ->where('documents.status', 3)
-                ->count();
-
-            $count_document = [
-                'total' => $total_document,
-                'active' => $total_active_document,
-                'waiting' => $total_waiting_document,
-                'expired' => $total_expired_document,
-            ];
-
-            // Mendapatkan data forecast document
-            $forecast_data = DB::table('document_schedules')
-            ->join('schedule_pics', 'document_schedules.id', '=', 'schedule_pics.document_schedule_id')
-            ->join('documents', 'document_schedules.document_id', '=', 'documents.id')
-            ->where('schedule_pics.user_pic_id', auth()->id())
-            ->where('documents.user_id', auth()->id())
-            ->select(
-                DB::raw('YEAR(schedule_date) AS year'),
-                DB::raw('MONTH(schedule_date) AS month'),
-                DB::raw('COUNT(DISTINCT documents.id) AS total_documents') // Menggunakan COUNT(DISTINCT) untuk menghitung dokumen yang unik
-            )
-            ->whereIn(DB::raw('YEAR(schedule_date)'), [date('Y'), date('Y') + 1])
-            ->groupBy(DB::raw('YEAR(schedule_date)'), DB::raw('MONTH(schedule_date)'))
-            ->orderBy(DB::raw('YEAR(schedule_date)'), 'asc')
-            ->orderBy(DB::raw('MONTH(schedule_date)'), 'asc')
-            ->get();
-        }
-
-        $grouped_forecast_data = [];
-        foreach ($forecast_data as $data) {
-            $grouped_forecast_data[$data->year][] = [
-                'month' => $data->month,
-                'total_documents' => $data->total_documents,
-            ];
-        }
-
-        logUserAccess("Dashboard-Filter");
-
-        return view(
-            "backend.index",
-            compact('departments', 'documenttypes', 'count_document', 'forecast_data', 'grouped_forecast_data')
-        );
     }
 
     public function calendar_event()
